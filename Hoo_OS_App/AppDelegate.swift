07bc5dfe83e720999697c167d9bd491aec05bff9
ryudain05 <dkdlsekdls@naver.com>
2022-01-24 09:20:58 +0900

    Initial Commit

---
+++ Hoo_OS_App/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Hoo_OS_App
+//
+//  Created by 류다인 on 2022/01/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Hoo_OS_App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hoo_OS_App
+//
+//  Created by 류다인 on 2022/01/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
